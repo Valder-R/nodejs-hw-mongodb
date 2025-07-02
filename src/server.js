e54b8313c557cf4c50f13e1736486fc8c149696b
Volodymyr_Rostovtsev <fortik1510@gmail.com>
2025-07-02 21:19:12 +0200

    implemented task

---
+++ src/server.js
@@ -0,0 +1,53 @@
+import express from "express";
+import pino from 'pino-http';
+import cors from "cors";
+import { getEnvVar } from "./utils/getEnvVar.js";
+import { getAllContacts, getContactById } from "./services/contacts.js";
+
+
+export function setupServer() {
+    const app = express();
+
+    app.use(pino({
+        transport: {
+            target: 'pino-pretty',
+        },
+    }),)
+
+    app.use(cors());
+
+    app.get('/contacts', async (req, res) => {
+        const contacts = await getAllContacts();
+        res.status(200).json({
+            message: "Successfully found contacts!",
+            data: contacts,
+        });
+    });
+
+    app.get('/contacts/:contactId', async (req, res) => {
+        const { contactId } = req.params;
+        const contact = await getContactById(contactId);
+        if (!contact) {
+            res.status(404).json({
+                message: 'Contact not found',
+            });
+            return;
+        }
+
+        res.status(200).json({
+            message: "Successfully found contact with id {contactId}!",
+            data: { ...contact },
+        });
+    });
+
+    app.use((req, res) => {
+        res.status(404).json({
+            message: 'Not found',
+        }
+        );
+    });
+    const PORT = getEnvVar("PORT", '3000');
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
