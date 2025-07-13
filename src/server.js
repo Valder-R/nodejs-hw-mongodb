5f3b695cac684cb9eb08f17cffb0c07e8faa460d
Volodymyr_Rostovtsev <fortik1510@gmail.com>
2025-07-13 20:54:13 +0200

    added routes

--- src/server.js
+++ src/server.js
@@ -2,53 +2,31 @@ import express from "express";
 import pino from 'pino-http';
 import cors from "cors";
 import { getEnvVar } from "./utils/getEnvVar.js";
-import { getAllContacts, getContactById } from "./services/contacts.js";
-
+import contactsRouter from "./routers/contacts.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
+import { notFoundHandler } from "./middlewares/notFoundHandler.js";
 
 export function setupServer() {
     const app = express();
+
+    app.use(express.json());
+
+    app.use(cors());
 
     app.use(pino({
         transport: {
             target: 'pino-pretty',
         },
-    }),)
+    }),);
 
-    app.use(cors());
+    app.use(contactsRouter);
 
-    app.get('/contacts', async (req, res) => {
-        const contacts = await getAllContacts();
-        res.status(200).json({
-            status: 200,
-            message: "Successfully found contacts!",
-            data: contacts,
-        });
-    });
+    app.use(notFoundHandler);
 
-    app.get('/contacts/:contactId', async (req, res) => {
-        const { contactId } = req.params;
-        const contact = await getContactById(contactId);
-        if (!contact) {
-            res.status(404).json({
-                message: 'Contact not found',
-            });
-            return;
-        }
+    app.use(errorHandler);
 
-        res.status(200).json({
-            status: 200,
-            message: "Successfully found contact with id {contactId}!",
-            data: contact ,
-        });
-    });
-
-    app.use((req, res) => {
-        res.status(404).json({
-            message: 'Not found',
-        }
-        );
-    });
     const PORT = getEnvVar("PORT", '3000');
+
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
